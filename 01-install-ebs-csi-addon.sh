82c4c562455c3af6bd1023bcd2533c5f006ed1db
aloda <aloda@aloda-MBA.local>
2023-11-24 11:35:24 +0800

    add install ebs csi addon script

--- 01-install-ebs-csi-addon.sh
+++ 01-install-ebs-csi-addon.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 
 #replace your eks cluster name
 clusterName=aloda-test
 #get oidc  endpoint
-aws eks describe-cluster \
-  --name $clusterName \      
-  --query "cluster.identity.oidc.issuer" \
-  --output text
+printf "正在获取eks 集群的oidc endpoint地址\n"
+
+aws eks describe-cluster --name $clusterName --query "cluster.identity.oidc.issuer" --output text
 
 #get oidc string
+printf "正在获取eks 集群的oidc字符串\n"
 oidc_id=$(aws eks describe-cluster --name $clusterName --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
 #check oidc enable or not, if return null that is not enable oidc
+printf "正在确认eks集群是否启用oidc \n"
 aws iam list-open-id-connect-providers | grep $oidc_id
-
 #enable oidc for eks cluster
+printf "正在为eks集群启用oidc \n"
 eksctl utils associate-iam-oidc-provider --cluster $clusterName --approve
 
